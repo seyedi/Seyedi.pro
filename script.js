4865157de76830a15af1d7d80d956815471a75f7
Seyed Mojtaba Seyedi <seyedi.mo@gmail.com>
2012-10-22 07:02:50 +0330

    making main page

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+		train = document.getElementById('trainn'),
+		currentSlide = 0;
+
+	nextSlide = function(){
+		go2slide(currentSlide+1);
+	}
+
+	prvSlide = function(){
+		go2slide(currentSlide-1);
+	}
+
+	document.getElementById('nxt').onclick=nextSlide;
+	document.getElementById('prv').onclick=prvSlide;
+}
+
+
+function go2slide(n){
+		train.style.left=(-950*n)+'px';
+		currentSlide=n;
+	}
