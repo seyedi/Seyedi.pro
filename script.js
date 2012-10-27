8f0845f287d75ce709c7dceb028dccaad4e64a36
Seyed Mojtaba Seyedi <seyedi.mo@gmail.com>
2012-10-27 23:41:08 +0330

    set home page and ...

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 window.onload=function(){
-		train = document.getElementById('trainn'),
-		currentSlide = 0;
+	train = document.getElementById('trainn'),
+	currentSlide = 0,
+	screenheight = screen.height,
+	next = document.getElementById('nxt'),
+	prev = document.getElementById('prv'),
+	lct  = train.style.left;
+	prev.style.top = ((screenheight/2)-200)+'px';
+	next.style.top = ((screenheight/2)-200)+'px';
 
 	nextSlide = function(){
+	// if (lct!=0) {
+	// 	prev.style.display = "block";
+	// }
 		go2slide(currentSlide+1);
+
 	}
 
 	prvSlide = function(){
 		go2slide(currentSlide-1);
 	}
 
-	document.getElementById('nxt').onclick=nextSlide;
-	document.getElementById('prv').onclick=prvSlide;
-}
+	next.onclick=nextSlide;
+	prev.onclick=prvSlide;
+	}
 
 
 function go2slide(n){
